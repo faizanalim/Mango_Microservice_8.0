b86052e403e0989cc539a348ea0cedffd5651ca1
faizanalim <faizan.23@hotmail.com>
2025-03-31 03:01:32 +0500

    Token Provider Service

---
+++ Mango.Web/Service/IService/ITokenProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Service.IService
+{
+    public interface ITokenProvider
+    {
+        void SetToken(string token);
+
+
+        string? GetToken();
+
+
+        void ClearToken();
+    }
+}
