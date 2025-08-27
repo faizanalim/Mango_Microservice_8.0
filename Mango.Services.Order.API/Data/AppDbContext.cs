e5e6fa3e439ac7bc9d24e25531f60b908b78e6c7
faizanalim <faizan.23@hotmail.com>
2025-08-27 05:04:48 +0500

    Order API Base Setup

---
+++ Mango.Services.Order.API/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Services.Order.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.Order.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+
+    }
+}
---
+++ Mango.Services.Order.API/Service/IService/IProductService.cs
@@ -0,0 +1,11 @@
+﻿
+using Mango.Services.Order.API.Models;
+
+
+namespace Mango.Services.Order.API.Service.IService
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+    }
+}
