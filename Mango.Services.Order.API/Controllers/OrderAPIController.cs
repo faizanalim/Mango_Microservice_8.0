d50173bcdd8eb990dfcff5e688276051556fcbb2
faizanalim <faizan.23@hotmail.com>
2025-12-26 04:49:18 +0500

    Create Stripe Session in Order API

--- Mango.Services.Order.API/Controllers/OrderAPIController.cs
+++ Mango.Services.Order.API/Controllers/OrderAPIController.cs
@@ -57,61 +57,49 @@ namespace Mango.Services.OrderAPI.Controllers
         }
 
         [Authorize]
-
-
-
         [HttpPost("CreateStripeSession")]
-
-
         public async Task<ResponseDto> CreateStripeSession([FromBody] StripeRequestDto stripeRequestDto)
-
-
         {
-
-
             try
-
-
             {
 
-
-
-
-
                 var options = new SessionCreateOptions
-
-
                 {
-
-
-                    SuccessUrl = "https://example.com/success",
-
-
-                    LineItems = new List<SessionLineItemOptions>
-
-
-                      {
-
-
-                        new SessionLineItemOptions
-
-
-                        {
-
-
-                          Price = "price_H5ggYwtDq4fbrJ",
-
-
-                          Quantity = 2,
-
-                        },
-                      },
+                    SuccessUrl = stripeRequestDto.ApprovedUrl,
+                    CancelUrl = stripeRequestDto.CancelUrl,
+                    LineItems = new List<SessionLineItemOptions>(),
                     Mode = "payment",
                 };
-                var service = new SessionService();
-                service.Create(options);
-            }
 
+                foreach (var item in stripeRequestDto.OrderHeader.OrderDetails)
+                {
+                    var sessionLineItem = new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = (long)(item.Price * 100), // $20.99 -> 2099
+                            Currency = "usd",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = item.Product.Name
+                            }
+                        },
+                        Quantity = item.Count
+                    };
+
+                    options.LineItems.Add(sessionLineItem);
+                }
+
+
+                var service = new SessionService();
+                Session session = service.Create(options);
+                stripeRequestDto.StripeSessionUrl = session.Url;
+                OrderHeader orderHeader = _db.OrderHeaders.First(u => u.OrderHeaderId == stripeRequestDto.OrderHeader.OrderHeaderId);
+                orderHeader.StripeSessionId = session.Id;
+                _db.SaveChanges();
+                _response.Result = stripeRequestDto;
+
+            }
             catch (Exception ex)
             {
                 _response.Message = ex.Message;
@@ -120,10 +108,5 @@ namespace Mango.Services.OrderAPI.Controllers
             return _response;
         }
 
-        //[HttpGet]
-        //public IActionResult Get()
-        //{
-        //    return Ok("Order API is working!");
-        //}
     }
 }
