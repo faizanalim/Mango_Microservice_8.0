39e6731231f657ccc7fd9c8f47ce2646cc4ff56e
faizanalim <faizan.23@hotmail.com>
2025-12-26 20:06:47 +0500

    Publish Message to Topic

--- Mango.Services.Order.API/Controllers/OrderAPIController.cs
+++ Mango.Services.Order.API/Controllers/OrderAPIController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Mango.Services.Order.API.Service.IService;
 using Stripe.Checkout;
 using Stripe;
+using MessageBus;
 
 namespace Mango.Services.OrderAPI.Controllers
 {
@@ -22,13 +23,17 @@ namespace Mango.Services.OrderAPI.Controllers
         private IMapper _mapper;
         private readonly AppDbContext _db;
         private IProductService _productService;
+        private readonly IMessageBus _messageBus;
+        private readonly IConfiguration _configuration;
         public OrderAPIController(AppDbContext db,
-            IProductService productService, IMapper mapper)
+            IProductService productService, IMapper mapper, 
+            IMessageBus messageBus)
         {
             _db = db;
             this._response = new ResponseDto();
             _productService = productService;
             _mapper = mapper;
+            _messageBus = messageBus;
         }
 
         [Authorize]
@@ -138,6 +143,19 @@ namespace Mango.Services.OrderAPI.Controllers
                     orderHeader.PaymentIntentId = paymentIntent.Id;
                     orderHeader.Status = SD.Status_Approved;
                     _db.SaveChanges();
+
+                    RewardsDto rewardsDto = new()
+                    {
+                        OrderId = orderHeader.OrderHeaderId,
+                        RewardsActivity = Convert.ToInt32(orderHeader.OrderTotal),
+                        UserId = orderHeader.UserId
+                    };
+
+
+                    string topicName = _configuration.GetValue<string>("TopicAndQueueNames:OrderCreatedTopic");
+
+
+                    await _messageBus.PublishMessage(rewardsDto, topicName);
 
                     _response.Result = _mapper.Map<OrderHeaderDto>(orderHeader);
                 }
---
+++ Mango.Services.Order.API/Models/Dto/RewardsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.Order.API.Models.Dto
+{
+    public class RewardsDto
+    {
+        public string UserId { get; set; }
+        public int RewardsActivity { get; set; }
+        public int OrderId { get; set; }
+    }
+}
