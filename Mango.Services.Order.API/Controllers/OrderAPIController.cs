feae372732ae3961922bb990d75489db93ff277a
faizanalim <faizan.23@hotmail.com>
2025-12-26 06:20:04 +0500

    Stripe Coupons and Order in Action

--- Mango.Services.Order.API/Controllers/OrderAPIController.cs
+++ Mango.Services.Order.API/Controllers/OrderAPIController.cs
@@ -70,7 +70,13 @@ namespace Mango.Services.OrderAPI.Controllers
                     LineItems = new List<SessionLineItemOptions>(),
                     Mode = "payment",
                 };
-
+                var DiscountsObj = new List<SessionDiscountOptions>()
+                {
+                    new SessionDiscountOptions
+                    {
+                        Coupon=stripeRequestDto.OrderHeader.CouponCode
+                    }
+                };
                 foreach (var item in stripeRequestDto.OrderHeader.OrderDetails)
                 {
                     var sessionLineItem = new SessionLineItemOptions
@@ -89,7 +95,10 @@ namespace Mango.Services.OrderAPI.Controllers
 
                     options.LineItems.Add(sessionLineItem);
                 }
-
+                if (stripeRequestDto.OrderHeader.Discount > 0)
+                {
+                    options.Discounts = DiscountsObj;
+                }
 
                 var service = new SessionService();
                 Session session = service.Create(options);
