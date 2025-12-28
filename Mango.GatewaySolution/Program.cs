1d4165f3a86de7d2185a20e426dad9137a9b1c75
faizanalim <faizan.23@hotmail.com>
2025-12-28 08:15:13 +0500

    Add Authentication to Ocelot

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -1,10 +1,15 @@
+using Mango.GatewaySolution.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.AddAppAuthetication();
 builder.Services.AddOcelot();
-app.MapGet("/", () => "Hello World!");
 
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
 app.UseOcelot();
 app.Run();
